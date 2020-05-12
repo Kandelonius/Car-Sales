b1b0a28037e779c63984bd2415eba0a276e9c57e
Shane Kaestner <shane.kaestner@yahoo.com>
2020-05-12 16:28:15 -0700

    updated the actions and am working on the onClick for AdditionalFeature

--- src/components/actions/optionsActions.js
+++ src/components/actions/optionsActions.js
@@ -1,26 +1,26 @@
 //👉 Build action creators
 
-export const TOGGLE_ENGINE = "TOGGLE_ENGINE";
-
 //actions creators are just functions that create (aka returns) actions
-export const toggleEngine = () => {
-  return { type: TOGGLE_ENGINE };
+export const ADD_ACTION = "ADD_ACTION";
+
+export const addAction = feature => {
+  return { type: ADD_ACTION, payload: feature};
 };
 
-export const TOGGLE_RACING = "TOGGLE_RACING";
+// export const TOGGLE_RACING = "TOGGLE_RACING";
 
-export const toggleRacing = () => {
-  return { type: TOGGLE_RACING };
-};
+// export const toggleRacing = () => {
+//   return { type: TOGGLE_RACING };
+// };
 
-export const TOGGLE_SOUND = "TOGGLE_SOUND";
+// export const TOGGLE_SOUND = "TOGGLE_SOUND";
 
-export const toggleSound = () => {
-  return { type: TOGGLE_SOUND };
-};
+// export const toggleSound = () => {
+//   return { type: TOGGLE_SOUND };
+// };
 
-export const TOGGLE_SPOILER = "TOGGLE_SPOILER";
+// export const TOGGLE_SPOILER = "TOGGLE_SPOILER";
 
-export const toggleSpoiler = () => {
-  return { type: TOGGLE_SPOILER };
-};
+// export const toggleSpoiler = () => {
+//   return { type: TOGGLE_SPOILER };
+// };
--- src/reducers/reducerNState.js
+++ src/reducers/reducerNState.js
@@ -1,24 +1,32 @@
 //👉 Set up "empty" reducer and initial state
 //👉 write the reducer logic for the actions
+import { ADD_ACTION } from "../components/actions/optionsActions";
 export const initialState = {
     additionalPrice: 0,
     car: {
         price: 26395,
         name: '2019 Ford Mustang',
         image:
-          'https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg',
+            'https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg',
         features: []
-      },
-      additionalFeatures: [
+    },
+    additionalFeatures: [
         { id: 1, name: 'V-6 engine', price: 1500 },
         { id: 2, name: 'Racing detail package', price: 1500 },
         { id: 3, name: 'Premium sound system', price: 500 },
         { id: 4, name: 'Rear spoiler', price: 250 }
-      ]
+    ]
 };
 
 export const optionsReducer = (state = initialState, action) => {
-    return {
-        ...state
+    console.log("action", action);
+    switch (action.type) {
+        case ADD_ACTION:
+            return {
+                ...state,
+                feature: action.payload
+            };
+        default:
+            return state;
     }
 };
